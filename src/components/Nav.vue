e4e8d21cb7b99b852a6fa3dfe8dbf7a12ce1f4a7
SabaKajaia <sabakajaia2@gmail.com>
2023-02-10 09:03:15 +0100

    Segundo

---
+++ src/components/Nav.vue
@@ -0,0 +1,87 @@
+<template>
+  <header class="bt-at-light-green text white">
+  <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+    <!-- <PersonalRouter :route="route" :buttonText="buttonText" class="logo-link"/> -->
+    <router-link to="/">
+      Home
+    </router-link>
+
+  <div class="flex items-center gap-x-4"></div>
+  <img class="w-14" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c516.png" alt="">
+  <h1 class="text-lg">Task Harmony</h1>
+    <ul>
+        <li>
+          <router-link to="/">Task Manager</router-link>
+        </li>
+
+        <li>
+          <router-link to="/account">Your Account</router-link>
+        </li>
+    </ul>
+
+    <div>
+      <ul>
+        <li class="log-out-welcome">
+          <p>Welcome, user</p>
+        </li>
+        <li>
+          <button @click="signOut" class="button">Log out</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <h1></h1>
+</header>
+</template>
+
+<script setup>
+// import PersonalRouter from "./PersonalRouter.vue";
+import { useUserStore } from "../stores/user";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+import { ref } from 'vue';
+
+//constant to save a variable that will hold the use router method
+const route = "/";
+const buttonText = "Todo app";
+
+// constant to save a variable that will get the user from store with a computed function imported from vue
+// const getUser = computed(() => useUserStore().user);
+const getUser = useUserStore().user;
+
+// constant that calls user email from the useUSerStore
+const userEmail = getUser.email;
+
+// async function that calls the signOut method from the useUserStore and pushes the user back to the Auth view.
+const redirect = useRouter();
+
+const signOut = async () => {
+  try{
+    // call the user store and send the users info to backend to signOut
+    // then redirect user to the homeView
+  } catch (error) {}
+};
+
+</script>
+
+<style>
+.navbar-img {
+  width: 90px;
+}
+
+nav {
+  background-color: lightgray;
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
+}
+
+nav ul {
+  list-style: none;
+  padding-inline-start: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,54 @@
+<!-- COMPONENTE BOILERPLATE -->
+ 
+<template>
+
+  <div class="container">
+    <div class="header">
+      <div class="header-description">
+        <h3 class="header-title">Sign In to ToDo App</h3>
+        <p class="header-subtitle">Get organized with your tasks!</p>
+      </div>
+    </div>
+
+    <form @submit.prevent="signIn" class="form-sign-in p-8 flex flex-col bg-light-grey rounded-md">
+      <div class="form flex flex-col mb-2">
+        <div class="form-input">
+          <label class="input-field-label mb-1 text-sm text-at-light-green">E-mail</label>
+          <input type="email" class="input-field p-2 text-gray-500 focus:outline-one" placeholder="example@gmail.com"
+            id="email" v-model="email" required />
+        </div>
+        <div class="form-input flex-col mb-2">
+          <label class="input-field-label">Password</label>
+          <input type="password" class="input-field p-2 text-gray-500 focus:outline-one" placeholder="**********"
+            id="password" v-model="password" required />
+        </div>
+        <button
+          class="button mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border border-transparent hover:border-white hover:bg-white hover:text-at-light-green"
+          type="submit">Sign In</button>
+        <p>
+          Don't have an account?
+          <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link" />
+        </p>
+      </div>
+    </form>
+
+    <div v-show="errorMsg">{{ errorMsg }}</div>
+  </div>
+</template>
+
+<script setup>
+import PersonalRouter from "./PersonalRouter.vue";
+
+// Route Variables
+const route = "/auth/signup";
+const buttonText = "Sign Up";
+
+// Arrow function to Signin user to supaBase
+const signIn = async () => {
+  try { } catch (error) { }
+};
+</script>
+
+<style>
+
+</style>
